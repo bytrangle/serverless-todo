ab9473101740a07305c762c01615defea197b8a0
Trang Le <lethutrang101@gmail.com>
2020-10-30 12:18:24 +0700

    Troubleshoot dashboard.js

--- packages/www/src/pages/dashboard.js
+++ packages/www/src/pages/dashboard.js
@@ -82,7 +82,7 @@ export default () => {
           </NavLink>
         )}
       </Flex>
-      <Flex
+      {/* <Flex
         as="form"
         onSubmit={async (e) => {
           e.preventDefault();
@@ -118,7 +118,7 @@ export default () => {
             ))}
           </ul>
         )}
-      </Flex>
+      </Flex> */}
     </Container>
   );
 };
